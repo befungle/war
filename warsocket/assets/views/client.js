65fc346082fd8a0eccbc7a373ded5aff1db2e113
Steven Burns <steven@stevenburns.net>
2016-05-11 15:15:07 -0400

    Style Update - Home Screen call out added.

--- warsocket/assets/views/client.js
+++ warsocket/assets/views/client.js
@@ -4,19 +4,19 @@
  * pageData defined in Master.js to store interactive page data
  */
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     pageData.init = 'n';
-    io.socket.on('users',function(resData) {
-        if (resData.verb == 'updated'){
+    io.socket.on('users', function (resData) {
+        if (resData.verb == 'updated') {
             getUsers();
             //console.log(resData);
         }
 
     });
     //Run check for local storage
-    if(lsTest() === true){
+    if (lsTest() === true) {
         // available
-        if (localStorage.getItem('userID')){
+        if (localStorage.getItem('userID')) {
             //Set global variable to stored value
             pageData.userID = localStorage.getItem('userID');
             gameInit();
@@ -26,8 +26,8 @@ jQuery(document).ready(function(){
         }
     } else {
         //Local storage unavailable - set "random"
-        var randID = getRandomInt(1000,100000);
-        pageData.userID = 'RA_'+randID;
+        var randID = getRandomInt(1000, 100000);
+        pageData.userID = 'RA_' + randID;
         createClient();
     }
     addToHomescreen.removeSession();
@@ -37,25 +37,25 @@ jQuery(document).ready(function(){
     });
 });
 
-function createClient(){
+function createClient() {
     io.socket.post('/Users/create/', {
         name: ''
     }, function (resData) {
         //resume operation
-        localStorage.setItem('userID',resData.id);
+        localStorage.setItem('userID', resData.id);
         pageData.userID = localStorage.getItem('userID');
         $('#clientRegistration').addClass('hideClass');
         gameInit();
     });
 }
 
-function lsTest(){
+function lsTest() {
     var test = 'test';
     try {
         localStorage.setItem(test, test);
         localStorage.removeItem(test);
         return true;
-    } catch(e) {
+    } catch (e) {
         return false;
     }
 }
@@ -65,30 +65,30 @@ function getRandomInt(min, max) {
 }
 
 
-function userData(){
+function userData() {
     $('#debugmessage').html('');
-    $('#debugmessage').prepend('User Id: '+pageData.userID+"<br>");
-    io.socket.get("/Users/"+pageData.userID,function(resData,jwres){
+    $('#debugmessage').prepend('User Id: ' + pageData.userID + "<br>");
+    io.socket.get("/Users/" + pageData.userID, function (resData, jwres) {
         pageData.userData = resData;
-        if (pageData.userData.name == ''){
+        if (pageData.userData.name == '') {
             $('#debugmessage').prepend('Warning, Not Named<br>');
             $('#clientRegistration').removeClass('hideClass');
         } else {
-            $('#debugmessage').prepend('Name: '+pageData.userData.name+"<br>");
+            $('#debugmessage').prepend('Name: ' + pageData.userData.name + "<br>");
             $('#clientRegistration').addClass('hideClass');
             $('#gameWindow').removeClass('hideClass');
-            if (pageData.init == 'n'){
-                setInterval('keepAlive()',1000);
+            if (pageData.init == 'n') {
+                setInterval('keepAlive()', 1000);
             }
         }
     });
 }
 
 
-function keepAlive(){
+function keepAlive() {
     pageData.init = 'y';
     var n = Date.now();
-    io.socket.post('/Users/update/'+pageData.userID, {
+    io.socket.post('/Users/update/' + pageData.userID, {
         lastactive: n
     }, function (resData) {
         //resume operation
@@ -97,20 +97,20 @@ function keepAlive(){
     getOpenGames();
 }
 
-function deregister(){
+function deregister() {
     localStorage.removeItem('userID');
     window.location.reload();
 }
 
 /* Custom to War */
-$('#deregister').click(function(){
+$('#deregister').click(function () {
     deregister();
 });
 
-$('#playerButton').click(function() {
-    $('#debugmessage').prepend('Name Entered: '+$('#playerName').val()+'<br>');
+$('#playerButton').click(function () {
+    $('#debugmessage').prepend('Name Entered: ' + $('#playerName').val() + '<br>');
     //Update Client Name
-    io.socket.post('/Users/update/'+pageData.userID, {
+    io.socket.post('/Users/update/' + pageData.userID, {
         name: $('#playerName').val()
     }, function (resData) {
         //resume operation
@@ -118,24 +118,24 @@ $('#playerButton').click(function() {
     });
 })
 
-function gameInit(){
+function gameInit() {
     userData();
     getUsers();
 }
 
-function getUsers(){
-    io.socket.get("/Users",function(resData,jwres){
+function getUsers() {
+    io.socket.get("/Users", function (resData, jwres) {
         pageData.users = resData;
         $('#players').html('');
         $('#players').html('<h3>All Players</h3>');
         pageData.activeusers = [];
-        $.each(pageData.users, function(){
+        $.each(pageData.users, function () {
             var n = Date.now();
-            if (this.lastactive){
-                var active = n-this.lastactive;
-                if (this.name != '' && active < 2000){
+            if (this.lastactive) {
+                var active = n - this.lastactive;
+                if (this.name != '' && active < 2000) {
                     pageData.activeusers.push(this.id);
-                    $('#players').append('<div class="columns large-12 medium-12 small-12">'+this.id+': '+this.name+' ('+active+')</div>');
+                    $('#players').append('<div class="columns large-12 medium-12 small-12">' + this.id + ': ' + this.name + ' (' + active + ')</div>');
                 }
             }
 

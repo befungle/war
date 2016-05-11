4c4eef6a3bf782e35ffaf088cbf08c1092463b86
Steven Burns <steven@stevenburns.net>
2016-05-11 15:34:59 -0400

    Style Update - Home Screen call out added.

--- warsocket/api/controllers/CardController.js
+++ warsocket/api/controllers/CardController.js
@@ -6,19 +6,19 @@
  */
 
 module.exports = {
-    sendTest: function(req,res){
-
-        //sails.sockets.blast('test');
-
-
-        sails.sockets.blast('user_logged_in', {
-            msg: 'User #' + req.session.userId + ' just logged in.',
-            user: {
-                id: req.session.userId,
-                username: req.session.username
-            }
-        }, req);
-        return res.send("hi there!");
-    },
+    //sendTest: function(req,res){
+    //
+    //    //sails.sockets.blast('test');
+    //
+    //
+    //    sails.sockets.blast('user_logged_in', {
+    //        msg: 'User #' + req.session.userId + ' just logged in.',
+    //        user: {
+    //            id: req.session.userId,
+    //            username: req.session.username
+    //        }
+    //    }, req);
+    //    return res.send("hi there!");
+    //},
 
 };
